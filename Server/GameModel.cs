9a1ed2592ec92e21505865c6b5e4152192063706
zanehirning <a02371568@usu.edu>
2024-04-05 23:57:22 -0600

    Correctly receiving messages, now build up the createNewEntity

--- Server/GameModel.cs
+++ Server/GameModel.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Audio;
+using System.Diagnostics;
 
 namespace Server
 {
