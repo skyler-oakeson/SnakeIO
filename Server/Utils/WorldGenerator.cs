7df684a2330930b058470f03526414f53960c1ab
zanehirning <a02371568@usu.edu>
2024-04-18 17:39:53 -0600

    Walls are correct - dont look at the math, it made sense at one point...

--- Server/Utils/WorldGenerator.cs
+++ Server/Utils/WorldGenerator.cs
@@ -8,7 +8,7 @@ namespace Utils
         private List<Shared.Entities.Entity> worldEntities = new List<Shared.Entities.Entity>();
         private const int TILE_SIZE = 750;
         private const int WALL_WIDTH = 50;
-        private const int WALL_HEIGHT = 50;
+        private const int WALL_HEIGHT = 15;
         private const int WORLD_SIZE = 1;
         private const int WORLD_ORIGIN = 0;
         private const int WORLD_END = 7500;
@@ -39,7 +39,7 @@ namespace Utils
                         {
                             int x = ((j * (TILE_SIZE)) + (w * WALL_WIDTH)) - (TILE_SIZE / 2) + (WALL_WIDTH / 2);
                             int y = (i * WALL_HEIGHT) - (TILE_SIZE / 2) - (WALL_HEIGHT / 2);
-                            Shared.Entities.Entity wall = Shared.Entities.Wall.Create("Images/player", Color.Green, new Rectangle(x, y, WALL_WIDTH, WALL_HEIGHT));
+                            Shared.Entities.Entity wall = Shared.Entities.Wall.Create("Images/square", Color.White, new Rectangle(x, y, WALL_HEIGHT, WALL_WIDTH));
                             worldEntities.Add(wall);
                             Server.MessageQueueServer.instance.broadcastMessage(new Shared.Messages.NewEntity(wall));
                         }
@@ -51,8 +51,8 @@ namespace Utils
                         for (int w = 0; w < wallCount; w++)
                         {
                             int x = ((j * (TILE_SIZE)) + (w * WALL_WIDTH)) - (TILE_SIZE / 2) + (WALL_WIDTH / 2);
-                            int y = ((i * WALL_HEIGHT) - (TILE_SIZE) - WALL_HEIGHT) + WORLD_END;
-                            Shared.Entities.Entity wall = Shared.Entities.Wall.Create("Images/player", Color.Green, new Rectangle(x, y, WALL_WIDTH, WALL_HEIGHT));
+                            int y = ((i * WALL_WIDTH) - (TILE_SIZE) - WALL_WIDTH) + WORLD_END - WALL_HEIGHT - 2;
+                            Shared.Entities.Entity wall = Shared.Entities.Wall.Create("Images/square", Color.White, new Rectangle(x, y, WALL_HEIGHT, WALL_WIDTH));
                             worldEntities.Add(wall);
                             Server.MessageQueueServer.instance.broadcastMessage(new Shared.Messages.NewEntity(wall));
                         }
@@ -66,7 +66,7 @@ namespace Utils
                         {
                             int x = (j * WALL_HEIGHT) - (TILE_SIZE / 2) - (WALL_HEIGHT / 2);
                             int y = ((i * (TILE_SIZE)) + (w * WALL_WIDTH)) - (TILE_SIZE / 2) + (WALL_WIDTH / 2);
-                            Shared.Entities.Entity wall = Shared.Entities.Wall.Create("Images/player", Color.Green, new Rectangle(x, y, WALL_WIDTH, WALL_HEIGHT));
+                            Shared.Entities.Entity wall = Shared.Entities.Wall.Create("Images/square", Color.White, new Rectangle(x, y, WALL_WIDTH, WALL_HEIGHT));
                             worldEntities.Add(wall);
                             Server.MessageQueueServer.instance.broadcastMessage(new Shared.Messages.NewEntity(wall));
                         }
@@ -77,9 +77,9 @@ namespace Utils
                         // For each tile, we need to render this many walls
                         for (int w = 0; w < wallCount; w++)
                         {
-                            int x = (j * WALL_HEIGHT) - (TILE_SIZE / 2) - (WALL_HEIGHT / 2) + WORLD_END - (TILE_SIZE / 2) - (WALL_WIDTH / 2);
+                            int x = (j * WALL_WIDTH) - (TILE_SIZE / 2) - (WALL_WIDTH / 2) + WORLD_END - (TILE_SIZE / 2) - (WALL_WIDTH / 2) - WALL_HEIGHT - 2;
                             int y = ((i * (TILE_SIZE)) + (w * WALL_WIDTH)) - (TILE_SIZE / 2) + (WALL_WIDTH / 2);
-                            Shared.Entities.Entity wall = Shared.Entities.Wall.Create("Images/player", Color.Green, new Rectangle(x, y, WALL_WIDTH, WALL_HEIGHT));
+                            Shared.Entities.Entity wall = Shared.Entities.Wall.Create("Images/square", Color.White, new Rectangle(x, y, WALL_WIDTH, WALL_HEIGHT));
                             worldEntities.Add(wall);
                             Server.MessageQueueServer.instance.broadcastMessage(new Shared.Messages.NewEntity(wall));
                         }
