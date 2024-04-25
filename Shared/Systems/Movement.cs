7a2050aca5ffb555e954d1c53cfbba67f19131b0
Soakeson <skyler.oakeson@gmail.com>
2024-04-25 09:39:27 -0600

    Refactored movement to be better organized.

--- Shared/Systems/Movement.cs
+++ Shared/Systems/Movement.cs
@@ -31,12 +31,50 @@ namespace Shared.Systems
             Shared.Components.Movable movable = entity.GetComponent<Shared.Components.Movable>();
             Shared.Components.Positionable positionable = entity.GetComponent<Shared.Components.Positionable>();
 
-            // Don't have to update if velocity is 0
-            if (movable.velocity.X == 0 && movable.velocity.Y == 0)
+            MovePosition(entity, elapsedTime);
+
+            // if it has camera, update camera center
+            if (entity.ContainsComponent<Shared.Components.Camera>())
             {
-                return;
+                MoveCamera(entity);
             }
 
+
+            // If Collidable update the hitbox position
+            if (entity.ContainsComponent<Shared.Components.Collidable>())
+            {
+                MoveHitBox(entity);
+            }
+        }
+
+        private void MoveCamera(Shared.Entities.Entity entity)
+        {
+            Shared.Components.Positionable positionable = entity.GetComponent<Shared.Components.Positionable>();
+            Shared.Components.Camera camera = entity.GetComponent<Shared.Components.Camera>();
+            camera.rectangle = new Rectangle((int)positionable.pos.X, (int)positionable.pos.Y, camera.rectangle.Width, camera.rectangle.Height);
+            camera.center = new Point((int)positionable.pos.X, (int)positionable.pos.Y);
+            camera.Follow(entity);
+        }
+
+        private void MoveHitBox(Shared.Entities.Entity entity)
+        {
+            Shared.Components.Positionable positionable = entity.GetComponent<Shared.Components.Positionable>();
+            Shared.Components.Collidable col = entity.GetComponent<Shared.Components.Collidable>();
+            if (col.Data.Shape == Shared.Components.CollidableShape.Circle)
+            {
+                col.Data.CircleData = new CircleData {x = positionable.pos.X, y = positionable.pos.Y, radius = col.Data.CircleData.radius};
+            }
+            if (col.Data.Shape == Shared.Components.CollidableShape.Rectangle)
+            {
+                col.Data.RectangleData = new RectangleData {x = positionable.pos.X, y = positionable.pos.Y, width = col.Data.RectangleData.width, height = col.Data.RectangleData.height };
+            }
+        }
+
+        private void MovePosition(Shared.Entities.Entity entity, TimeSpan elapsedTime)
+        {
+            Shared.Components.Movable movable = entity.GetComponent<Shared.Components.Movable>();
+            Shared.Components.Positionable positionable = entity.GetComponent<Shared.Components.Positionable>();
+            // If entity is not the head don't need to update it
             if (entity.ContainsComponent<Shared.Components.Linkable>())
             {
                 if (entity.GetComponent<Shared.Components.Linkable>().linkPos != LinkPosition.Head)
@@ -44,37 +82,18 @@ namespace Shared.Systems
                     return;
                 }
             }
+            // Don't have to update if velocity is 0
+            if (movable.velocity == new Vector2(0, 0))
+            {
+                return;
+            }
             // Cap velocity
             movable.velocity = new Vector2(Math.Clamp(movable.velocity.X, -1, 1), Math.Clamp(movable.velocity.Y, -1, 1));
             movable.velocity = (movable.velocity / movable.velocity.Length()) * .5f; // multiple by .5f for speed, and some other oddities
 
             Vector2 newpos = (movable.velocity) * elapsedTime.Milliseconds + positionable.pos;
             positionable.UpdatePosition(newpos);
-
-            // if it has camera, update camera center
-            if (entity.ContainsComponent<Shared.Components.Camera>())
-            {
-                Shared.Components.Camera camera = entity.GetComponent<Shared.Components.Camera>();
-                camera.rectangle = new Rectangle((int)positionable.pos.X, (int)positionable.pos.Y, camera.rectangle.Width, camera.rectangle.Height);
-                camera.center = new Point((int)positionable.pos.X, (int)positionable.pos.Y);
-                camera.Follow(entity);
-            }
-
             positionable.orientation = (float)Math.Atan(movable.velocity.Y / movable.velocity.X);
-
-            // If Collidable update the hitbox position
-            if (entity.ContainsComponent<Shared.Components.Collidable>())
-            {
-                Shared.Components.Collidable col = entity.GetComponent<Shared.Components.Collidable>();
-                if (col.Data.Shape == Shared.Components.CollidableShape.Circle)
-                {
-                    col.Data.CircleData = new CircleData {x = newpos.X, y = newpos.Y, radius = col.Data.CircleData.radius};
-                }
-                if (col.Data.Shape == Shared.Components.CollidableShape.Rectangle)
-                {
-                    col.Data.RectangleData = new RectangleData {x = newpos.X, y = newpos.Y, width = col.Data.RectangleData.width, height = col.Data.RectangleData.height };
-                }
-            }
         }
     }
 }
