ff91523b4587bce4a28f9a0454b0466c358b46b0
Robert Gordon <darkmono37@gmail.com>
2024-04-25 17:37:29 -0600

    Base

--- SnakeIO/Scenes/CreditScene.cs
+++ SnakeIO/Scenes/CreditScene.cs
@@ -3,28 +3,37 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using System;
 using Microsoft.Xna.Framework.Input;
+using Systems;
+using System.Collections.Generic;
+
 
 namespace Scenes
 {
     public class CreditScene : Scene
     {
-        private Systems.Renderer renderer;
-        private Systems.KeyboardInput keyboardInput;
-                private Systems.Audio audio;
-        private ContentManager contentManager;
+        private Renderer renderer;
+        private KeyboardInput keyboardInput;
+        private Selector<SceneContext> selector;
+        private Audio audio;
+        private SpriteFont font;
+
         public CreditScene(GraphicsDevice graphicsDevice, GraphicsDeviceManager graphics, Shared.Controls.ControlManager controlManager)
         {
-           
             this.Initialize(graphicsDevice, graphics, controlManager);
             this.controlManager = controlManager;
             this.keyboardInput = new Systems.KeyboardInput(controlManager);
-            this.renderer = new Systems.Renderer(spriteBatch);
-            this.audio = new Systems.Audio();
-
+            this.renderer = new Renderer(spriteBatch);
+            this.audio = new Audio();
+           
         }
 
         override public void LoadContent(ContentManager contentManager)
         {
+
+            int center = graphics.PreferredBackBufferWidth / 2;
+            font = contentManager.Load<SpriteFont>("Fonts/Micro5-50");
+            AddEntity(Shared.Entities.StaticText.Create(font, "Credits", Color.Black, Color.Orange, new Rectangle(center - (int)font.MeasureString("Credits").X / 2, 50 + (int)font.MeasureString("Credits").Y / 2, 0, 0)));
+
 
         }
 
@@ -32,22 +41,37 @@ namespace Scenes
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             {
+                
                 return SceneContext.MainMenu;
             }
 
             return SceneContext.Credits;
         }
 
+
         override public void Render(TimeSpan elapsedTime)
         {
-
+            renderer.Update(elapsedTime);
         }
 
         override public void Update(TimeSpan elapsedTime)
         {
 
+            renderer.Update(elapsedTime);
+            keyboardInput.Update(elapsedTime);
+            audio.Update(elapsedTime);
         }
 
+        private void AddEntity(Shared.Entities.Entity entity)
+        {
+            renderer.Add(entity);
+            keyboardInput.Add(entity);
+            audio.Add(entity);
+
+        }
+
+
+       
     }
 }
 
